41c1cfdedecfab9285a68636f2aebd5a24c32582
GeekyHacks <geekyhacks22@gmail.com>
2023-08-30 15:08:18 +0300

    edit deploy

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,30 @@ function App() {
   return (
     <div className="App">
       <nav className="navBar">
-        <Link className="bookstoreTitle" to="/BookStore_React/">
+        <Link className="bookstoreTitle" to="/BookStoreReact_Vite/">
           Geeky Book Store
         </Link>
         <div className="navLinks">
-          <Link className="link" to="/BookStore_React/books">
+          <Link className="link" to="/BookStoreReact_Vite/books">
             Books
           </Link>
-          <Link className="link" to="/BookStore_React/categories">
+          <Link className="link" to="/BookStoreReact_Vite/categories">
             Categories
           </Link>
-          <Link className="link" to="/BookStore_React/contact">
+          <Link className="link" to="/BookStoreReact_Vite/contact">
             Contact
           </Link>
-          <Link className="bookstoreTitle" to="/BookStore_React/">
+          <Link className="bookstoreTitle" to="/BookStoreReact_Vite/">
             <img className="accountPic" src={accountPic} alt="" />
           </Link>
         </div>
       </nav>
       <Routes className="Routes">
-        <Route path="/BookStore_React/" element={<BooksContainer />} />
-        <Route path="/BookStore_React/books" element={<BooksContainer />} />
-        <Route path="/BookStore_React/contact" element={<Contact />} />
-        <Route path="/BookStore_React/*" element={<div className="notFound"> Page not found!! </div>} />
-        <Route path="/BookStore_React/categories" element={<CatContainer />} />
+        <Route path="/BookStoreReact_Vite/" element={<BooksContainer />} />
+        <Route path="/BookStoreReact_Vite/books" element={<BooksContainer />} />
+        <Route path="/BookStoreReact_Vite/contact" element={<Contact />} />
+        <Route path="/BookStoreReact_Vite/*" element={<div className="notFound"> Page not found!! </div>} />
+        <Route path="/BookStoreReact_Vite/categories" element={<CatContainer />} />
       </Routes>
       <Footer />
     </div>
